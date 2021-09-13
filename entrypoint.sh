464addd7d10e792c5d4c4b4e81987b9468191239
Artur M. Wolff <artur@storj.io>
2021-09-13 18:12:10 +0200

    update and sort list of enabled/disabled tests
    Change-Id: Iaddb9f4c8531c199350924b416eaac7517ab1a10

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,8 +17,8 @@
 
 if [ $# -eq 0 ]
 then
-    ./mint.sh aws-sdk-go aws-sdk-php awscli minio-go aws-sdk-java aws-sdk-ruby s3cmd s3select &
-    # these tests are disabled in their entirety by STORJ: healthcheck mc minio-dotnet minio-java minio-js minio-py
+    ./mint.sh aws-sdk-go aws-sdk-java aws-sdk-php aws-sdk-ruby awscli minio-go s3cmd s3select &
+    # these tests are disabled in their entirety by STORJ: healthcheck mc minio-dotnet minio-java minio-js minio-py versioning
 else
     ./mint.sh "$@" &
 fi
