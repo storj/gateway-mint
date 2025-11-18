e777e460fe707d3b98697c283983c6df2c55afc8
Jeremy Wharton <jeremy.e.wharton@gmail.com>
2025-11-18 00:45:37 -0600

    entrypoint.sh,jenkins: fix indentation issues
    This change adjusts shell files that use spaces for indentation where
    tabs are expected.
    
    Change-Id: I6add8fa121a40afe270cc4766af41e0cd6652484

--- jenkins/start_storj-sim.sh
+++ jenkins/start_storj-sim.sh
@@ -3,14 +3,14 @@
 set -euo pipefail
 
 go install -race -v \
-    storj.io/storj/cmd/satellite@latest \
-    storj.io/storj/cmd/storagenode@latest \
-    storj.io/storj/cmd/storj-sim@latest \
-    storj.io/storj/cmd/versioncontrol@latest \
-    storj.io/storj/cmd/uplink@latest \
-    storj.io/storj/cmd/identity@latest \
-    storj.io/storj/cmd/certificates@latest \
-    storj.io/storj/cmd/multinode@latest
+	storj.io/storj/cmd/satellite@latest \
+	storj.io/storj/cmd/storagenode@latest \
+	storj.io/storj/cmd/storj-sim@latest \
+	storj.io/storj/cmd/versioncontrol@latest \
+	storj.io/storj/cmd/uplink@latest \
+	storj.io/storj/cmd/identity@latest \
+	storj.io/storj/cmd/certificates@latest \
+	storj.io/storj/cmd/multinode@latest
 
 go install -race -v storj.io/gateway@latest
 
@@ -19,8 +19,8 @@ echo "=== Finished installing dependencies"
 echo "=== Setting up storj-sim..."
 
 until storj-sim -x --host sim network setup; do
-    echo "*** redis/postgres is not yet available; waiting for 3s..."
-    sleep 3
+	echo "*** redis/postgres is not yet available; waiting for 3s..."
+	sleep 3
 done
 
 sed -i 's/# metainfo.rate-limiter.enabled: true/metainfo.rate-limiter.enabled: false/g' "$(storj-sim network env SATELLITE_0_DIR)/config.yaml"
