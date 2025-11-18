b7e97286e1ca301d39f907429200be9c41f6880a
Jeremy Wharton <jeremy.e.wharton@gmail.com>
2025-11-18 00:45:37 -0600

    entrypoint.sh,jenkins: fix indentation issues
    This change adjusts shell files that use spaces for indentation where
    tabs are expected.
    
    Change-Id: I6add8fa121a40afe270cc4766af41e0cd6652484

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,10 +17,10 @@
 
 if [ $# -eq 0 ]
 then
-    ./mint.sh aws-sdk-go aws-sdk-java aws-sdk-php aws-sdk-ruby awscli minio-go s3cmd s3select &
-    # these tests are disabled in their entirety by STORJ: healthcheck mc minio-dotnet minio-java minio-js minio-py versioning
+	./mint.sh aws-sdk-go aws-sdk-java aws-sdk-php aws-sdk-ruby awscli minio-go s3cmd s3select &
+	# these tests are disabled in their entirety by STORJ: healthcheck mc minio-dotnet minio-java minio-js minio-py versioning
 else
-    ./mint.sh "$@" &
+	./mint.sh "$@" &
 fi
 
 # Get the pid to be used for kill command if required
